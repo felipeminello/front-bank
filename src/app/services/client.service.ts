15be933c5ca21c642069e52e1c8585f7e6dc2d1d
Felipe Minello <felipeminello@gmail.com>
2023-05-14 21:10:14 -0300

    feat: add env file

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,28 +1,33 @@
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Client } from '../../shared/models/client.interface';
-import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
+  private apiUrl = environment.apiUrl;
+
   constructor(public http: HttpClient) {}
 
   postClient(client: Client): Observable<Client> {
-    return this.http.post<Client>('http://localhost:3000/clientList', client);
+    return this.http.post<Client>(`${this.apiUrl}/clientList`, client);
   }
 
   getClient(): Observable<Client[]> {
-    return this.http.get<Client[]>('http://localhost:3000/clientList');
+    return this.http.get<Client[]>(`${this.apiUrl}/clientList`);
   }
+
   putClient(client: Client, id: number) {
     return this.http.put<Client>(
-      'http://localhost:3000/clientList/' + id,
+      `${this.apiUrl}/clientList/${id}`,
       client
     );
   }
+
   deleteClient(id: number) {
-    return this.http.delete<Client>('http://localhost:3000/clientList/' + id);
+    return this.http.delete<Client>(`${this.apiUrl}/clientList/${id}`);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://192.168.0.5:3000'
+};
